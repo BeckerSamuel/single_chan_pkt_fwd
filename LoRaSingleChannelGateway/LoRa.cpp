adccda8087b3a137479a7374c40be8815376995d
Samuel <becker-samu@gmx.de>
2024-03-29 17:48:25 +0100

    some more adjustments to the Makefile

--- LoRaSingleChannelGateway/LoRa.cpp
+++ LoRaSingleChannelGateway/LoRa.cpp
@@ -2,8 +2,8 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "LoRa.h"
-#include "wiringPiSPIAdjusted.h"
 #include <wiringPi.h>
+#include <wiringPi/wiringPiSPIAdjusted.h>
 #include <thread>
 
 // registers
